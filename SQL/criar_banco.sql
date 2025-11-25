fec7c1f532d0d54b4adee27cff5c42eee9f6ee16
unknown <lab1062@etecjk.local>
2025-11-25 19:55:49 -0300

    mt

--- SQL/criar_banco.sql
+++ SQL/criar_banco.sql
@@ -9,43 +9,12 @@ CREATE TABLE Planos (
     Valor_plano DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE Estado (
-id_estado INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(200) NOT NULL,
-sigla CHAR(2) NOT NULL UNIQUE
-);
 
-CREATE TABLE Cidade (
-id_cidade INT AUTO_INCREMENT PRIMARY KEY,
-estado_id_estado INT NOT NULL,
-nome VARCHAR(200) NOT NULL,
-UNIQUE (nome, estado_id_estado),
-CONSTRAINT fk_estado FOREIGN KEY (estado_id_estado) REFERENCES Estado(id_estado)
-);
-
-CREATE TABLE Bairro (
-id_bairro INT AUTO_INCREMENT PRIMARY KEY,
-cidade_id_cidade INT NOT NULL,
-nome VARCHAR(200) NOT NULL,
-UNIQUE (nome, cidade_id_cidade),
-CONSTRAINT fk_cidade FOREIGN KEY (cidade_id_cidade) REFERENCES Cidade(id_cidade)
-);
-
-CREATE TABLE Logradouro (
-id_logradouro INT AUTO_INCREMENT PRIMARY KEY,
-bairro_id_bairro INT NOT NULL,
-nome VARCHAR(200) NOT NULL,
-tipo VARCHAR(50) NOT NULL,
-UNIQUE (nome, bairro_id_bairro),
-CONSTRAINT fk_bairro FOREIGN KEY (bairro_id_bairro) REFERENCES Bairro(id_bairro)
-);
 
 CREATE TABLE Unidades (
     id_unidades INT AUTO_INCREMENT PRIMARY KEY,
-	logradouro_id_logradouro INT NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    complemento VARCHAR(100),
-    telefone VARCHAR(20),
+	CEP VARCHAR(255) NOT NULL,
+      telefone VARCHAR(20),
     cnpj_unidade VARCHAR(20) UNIQUE,
     CONSTRAINT fk_enderecoUni FOREIGN KEY (logradouro_id_logradouro) REFERENCES Logradouro (id_logradouro) 
 );
