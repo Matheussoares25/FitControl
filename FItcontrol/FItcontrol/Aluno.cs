87ba8450c5316a190b414bcdca4497b4b7a626ca
unknown <lab1063@etecjk.local>
2025-11-25 20:06:05 -0300

    Classe Aluno criado

---
+++ FItcontrol/FItcontrol/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FItcontrol
+{
+
+    public enum genero
+    {
+        Masculino,
+        Feminino
+    }
+    public class Aluno
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+        public genero Genero { get; set; }
+        public string Matricula { get; set; }
+
+    }
+}
