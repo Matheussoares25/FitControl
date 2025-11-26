7d2487c05632b3a2031531f900f1d4852a49ab55
unknown <lab1061@etecjk.local>
2025-11-25 21:16:09 -0300

    menu

---
+++ FItcontrol/FItcontrol/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FItcontrol
+{
+     class Menu
+    {
+        public string Unidades {  get; set; }
+        public string Alunos { get; set; }
+
+        public string Planos { get; set; }
+
+        public void Hud()
+        {
+            Console.WriteLine("========= BEM VINDO AO FIT CONTROL =========\n");
+            Console.WriteLine("Selecione a opcao:\n");
+            Console.WriteLine("1. Planos\n");
+            string res = Console.ReadLine();
+        }
+    }
+}
